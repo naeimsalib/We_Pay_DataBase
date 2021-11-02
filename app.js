5be82f78037f2aa630e80770db472d4144d0aafc
Ivan Lei <ilei0893@gmail.com>
2021-11-01 22:35:56 -0400

    Added routes to main app file

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
 const express = require("express");
 const app = express();
-var cors = require('cors');
+var cors = require("cors");
 const sql = require("mysql");
 
 //allows for middleware to work
 app.use(cors());
+app.use(express.json());
 
 //connect to database (pls modify user and password if needed to connect locally)
 const con = sql.createConnection({
   host: "localhost",
   user: "root",
-  password: "123",
-  database: "wepay"
-})
+  password: "password",
+  database: "wepay",
+});
 
 //connect to the database
 con.connect((err) => {
   if (err) throw err;
-  console.log("DB Connected.")
-})
-
-//get every employee
-app.get("/employees", function (req, res) {
-  con.query("SELECT * FROM employee", function (err, result, fields){
-    if(err) throw err;
-    console.log(result)
-    res.send(result);
-  }) 
-
+  console.log("DB Connected.");
 });
 
+app.get("/benefits", function (req, res) {
+  con.query("SELECT * FROM employee_benefits", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    res.send(result);
+  });
+});
+
+app.get("/employees", function (req, res) {
+  con.query("SELECT * FROM employee", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    res.send(result);
+  });
+});
 const startListening = () => {
   const PORT = 3001;
   app.listen(PORT, () => {
