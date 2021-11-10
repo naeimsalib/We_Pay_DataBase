a5f7acd52fd34736b78af200a079a5b96ad8ecb9
Christopher Artrip <christopherartrip@christophersmbp.home>
2021-11-10 15:03:56 -0500

    changed Salary from VarChar to int

--- WePayDataBase.sql
+++ WePayDataBase.sql
@@ -5,7 +5,7 @@ USE wepay;
 CREATE TABLE employee (
 eID int NOT NULL PRIMARY KEY AUTO_INCREMENT, /* Holds the Person ID*/
 Name VarChar(255), /* holds the Employee Name*/
-Salary VarChar(255),/*Holds the Employee Salary*/
+Salary int,/*Holds the Employee Salary*/
 Status int, /* Holds the Employee Work Status, 0 for not working , 1 for working*/
 StartDate DATE, /*Holds the employee hiring date*/
 Position VarChar(255), /*Holds there position*/
@@ -22,7 +22,7 @@ SSN VarChar(255) /*Holds the Employee SSN*/
 CREATE TABLE past_employee (
 peID int NOT NULL PRIMARY KEY AUTO_INCREMENT, /* Holds the Person ID*/
 Name VarChar(255), /* holds the Employee Name*/
-Salary VarChar(255),/*Holds the Employee Salary*/
+Salary int,/*Holds the Employee Salary*/
 StartDate DATE, /*Holds the employee hiring date*/
 Position VarChar(255), /*Holds there position*/
 Phone_Number VarChar(255), /*Holds the employee Phone Number*/
