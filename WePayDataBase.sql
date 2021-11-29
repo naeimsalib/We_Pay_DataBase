7b9e66b946930a118e8fd033fd57e0ce4317e3a2
Naeim Salib <naiemsalib@Yahoo.com>
2021-11-29 17:27:10 -0500

    updated Employee table
    added Hourly_Rate, which is an int that holds the employee Hourly rate

--- WePayDataBase.sql
+++ WePayDataBase.sql
@@ -6,11 +6,12 @@ CREATE TABLE employee (
 eID int NOT NULL PRIMARY KEY AUTO_INCREMENT, /* Holds the Person ID*/
 Name VarChar(255), /* holds the Employee Name*/
 Salary int,/*Holds the Employee Salary*/
-Email VarChar(255),  
+Email VarChar(255),
 Status int, /* Holds the Employee Work Status, 0 for not working , 1 for working*/
 StartDate DATE, /*Holds the employee hiring date*/
 Position VarChar(255), /*Holds there position*/
 Manager int, /* 0 for False, and 1 for True*/
+Hourly_Rate int, /*Holds the employee Hourly rate*/
 Address VarChar(255), /*Holds there address*/
 WorkState VarChar(255), /*Holds there work station*/
 LivingState VarChar(255),/*Holds were does the employee works from(State)*/
